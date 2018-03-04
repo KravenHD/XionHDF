a3f60010410f0b4b45efe8899f140383d47ee0cc
Henry Schrapel <dende@hdfreaks.cc>
2018-03-04 16:48:07 +0100

    Add Axas E4HD 4K Ultra HD

--- usr/lib/enigma2/python/Plugins/Extensions/XionHDF/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/XionHDF/plugin.py
@@ -204,7 +204,7 @@ else:
    mem_info = []
    entrie = os.popen('cat /proc/cmdline').read()
 
-   if getBoxType() in ('vusolo4k', 'mutant51', 'mutant52', 'ax51', 'zgemmah7'):
+   if getBoxType() in ('vusolo4k', 'mutant51', 'mutant52', 'ax51', 'zgemmah7', 'e4hdultra'):
         mem = re.findall('_cma=(.*?)M', entrie)
    else:   
         mem = re.findall('bmem=(.*?)M', entrie)
@@ -215,7 +215,7 @@ else:
    if len(mem_info) > 1:
       bmem = int(mem_info[0]) + int(mem_info[1])  
    else:
-      if getBoxType() in ('sf4008', 'dinobot4k', 'anadol4k', 'gbquad4k', 'gbue4k'):
+      if getBoxType() in ('sf4008', 'dinobot4k', 'anadol4k', 'gbquad4k', 'gbue4k', 'e4hdultra'):
          bmem = 250
       else:
          bmem = int(mem_info[0])
