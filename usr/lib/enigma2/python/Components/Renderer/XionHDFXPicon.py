9f7e435e79efb1b347e4a8ee699717df5d91937c
koivo <koivo@live.de>
2019-01-22 19:26:04 +0100

    update picons

--- usr/lib/enigma2/python/Components/Renderer/XionHDFXPicon.py
+++ usr/lib/enigma2/python/Components/Renderer/XionHDFXPicon.py
@@ -45,11 +45,7 @@ class XionHDFXPicon(Renderer):
 					pngname = self.findPicon(sname)
 					if pngname == "":
 						fields = sname.split('_', 3)
-						if len(fields) > 2 and fields[2] != '2': #fallback to 1 for tv services with nonstandard servicetypes
-							fields[2] = '1'
-						if fields[0] == '4097': #fallback to 1 for IPTV streams
-							fields[0] = '1'
-						if fields[0] == '5002': #fallback to 1 for IPTV streams
+						if len(fields) > 0 and fields[0] != '1':
 							fields[0] = '1'
 						pngname = self.findPicon('_'.join(fields))
 					if not pngname: # picon by channel name
