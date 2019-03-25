7805aaff2e6a9bba0a5a6109f837ebe1fdc0f014
koivo <koivo@live.de>
2019-03-25 13:41:01 +0100

    add some temp sensors

--- usr/lib/enigma2/python/Components/Renderer/XionHDFSYSTemp.py
+++ usr/lib/enigma2/python/Components/Renderer/XionHDFSYSTemp.py
@@ -20,9 +20,25 @@ class XionHDFSYSTemp(Renderer, VariableText):
 				elif path.exists('/proc/stb/fp/temp_sensor'):
 					out_line = popen("cat /proc/stb/fp/temp_sensor").readline()
 					systemp = out_line.replace('\n', '')
+				elif path.exists('/proc/stb/sensors/temp/value'):
+					out_line = popen("cat /proc/stb/sensors/temp/value").readline()
+					systemp = out_line.replace('\n', '')
+				elif path.exists('/proc/stb/power/avs'):
+					out_line = popen("cat /proc/stb/power/avs").readline()
+					systemp = out_line.replace('\n', '')
 				elif path.exists('/proc/stb/fp/temp_sensor_avs'):
 					out_line = popen("cat /proc/stb/fp/temp_sensor_avs").readline()
 					systemp = out_line.replace('\n', '').replace(' ','')
+				elif path.exists('/proc/hisi/msp/pm_cpu'):
+					try:
+						for line in open('/proc/hisi/msp/pm_cpu').readlines():
+							line = [x.strip() for x in line.strip().split(":")]
+							if line[0] in ("Tsensor"):
+								temp = line[1].split("=")
+								temp = line[1].split(" ")
+								tempinfo = temp[2]
+					except:
+						tempinfo = ""
 				elif path.exists('/sys/devices/virtual/thermal/thermal_zone0/temp'):
 					out_line = open('/sys/devices/virtual/thermal/thermal_zone0/temp', 'r')
 					systemp = out_line.read()
